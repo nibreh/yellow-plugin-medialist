c6fd0b9b394e6bbc99c1f4346bc299134150a6a8
nibreh <nibreh@legtux.org>
2015-01-29 09:13:06 +0100

    Remove $size variable
    The `$size` variable is unnecessary

--- musiclist.php
+++ musiclist.php
@@ -1,7 +1,6 @@
-<?php list($name, $pattern, $style, $size) = $yellow->getSnippetArgs() ?>
+<?php list($name, $pattern, $style) = $yellow->getSnippetArgs() ?>
 <?php if(!$pattern) $pattern = basename($yellow->page->location).".(ogg|mp3)" ?>
 <?php if(!$style) $style = "musiclist" ?>
-<?php if(!$size) $size = "100%" ?>
 <ul class="<?php echo htmlspecialchars($style) ?>">
 <?php $path = dirname($pattern); $path = ($path!=".") ? $path."/" : ""; $regex = "/^".basename($pattern)."$/"; ?>
 <?php foreach($yellow->toolbox->getDirectoryEntries($yellow->config->get("mediaDir").$path, $regex, true, false, false) as $fileName): ?>
