09c7b1d8e38f3700d82d701c9a4b01e4bfe13f83
nibreh <nibreh@users.noreply.github.com>
2016-02-08 14:30:38 +0100

    Update medialist.php

--- medialist.php
+++ medialist.php
@@ -4,7 +4,7 @@
 // Medialist plugin
 class YellowMedialist
 {
-	const Version = "0.5.3";
+	const Version = "0.5.4";
 	var $yellow;			//access to API
 	// Handle initialisation
 	function onLoad($yellow)
@@ -27,25 +27,28 @@ class YellowMedialist
 			{
 				$page->setLastModified($files->getModified());
 				$output = "<div class=\"".htmlspecialchars($style)."\">\n";
+                    		$output .= "<ul>";
 				foreach($files as $file)
 				{
-                    $output .= "<p>";
+
 					switch($file->get("type"))
 					{
-						case "mp3":	$output .= "<audio controls=\"controls\">\n";
-									$output .= "<source src=\"".htmlspecialchars($file->getLocation())."\" type=\"audio/mp3\" />\n";
-									$output .= "</audio><br>\n";
+						case "mp3":	$output .= "<li><audio controls=\"controls\">\n";
+								$output .= "<source src=\"".htmlspecialchars($file->getLocation())."\" type=\"audio/mp3\" />\n";
+								$output .= "</audio><br>\n";
+	                					$output .= "<a href=\"".htmlspecialchars($file->getLocation())."\">".htmlspecialchars(basename($file->fileName))."</a></li>\n";
 									break;
 						case "ogg":	$output .= "<audio controls=\"controls\">";
-									$output .= "<source src=\"".htmlspecialchars($file->getLocation())."\" type=\"audio/ogg\" />\n";
-									$output .= "</audio><br>\n";
+								$output .= "<source src=\"".htmlspecialchars($file->getLocation())."\" type=\"audio/ogg\" />\n";
+								$output .= "</audio><br>\n";
+	                					$output .= "<a href=\"".htmlspecialchars($file->getLocation())."\">".htmlspecialchars(basename($file->fileName))."</a></p>\n";
 									break;
-                        case "pdf":	$output .= "<b>PDF Document:</b> \n";
+                        			case "pdf":	$output .= "<li><a href=\"".htmlspecialchars($file->getLocation())."\">".htmlspecialchars(basename($file->fileName))."</a></li>\n";
 									break;
 					}
-	                $output .= "<a href=\"".htmlspecialchars($file->getLocation())."\">".htmlspecialchars(basename($file->fileName))."</a></p>\n";
+
 				}
-				$output .= "</div>\n";
+				$output .= "</ul></div>\n";
 			} else {
 				$page->error(500, "Medialist '$pattern' does not exist!");
 			}
