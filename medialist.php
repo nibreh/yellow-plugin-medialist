82bcc121cd79488687a33cd5ca6646e6db83c1ae
Juh <nibreh@users.noreply.github.com>
2018-10-07 17:58:34 +0200

    Update to 0.7.7

--- medialist.php
+++ medialist.php
@@ -1,39 +1,32 @@
 <?php
-// Copyright (c) 2013-2015 Datenstrom, http://datenstrom.se
+// Medialist plugin https://github.com/nibreh/yellow-plugin-medialist
+// Copyright (c) 2018 Nibreh & Markseu
 // This file may be used and distributed under the terms of the public license.
 
-// Medialist plugin by nibreh and markseu
-class YellowMedialist
-{
-	const VERSION = "0.6.4";
-	var $yellow;			//access to API
+class YellowMedialist {
+	const VERSION = "0.7.7";
+	public $yellow;			//access to API
+	
 	// Handle initialisation
-	function onLoad($yellow)
-	{
+	public function onLoad($yellow) {
 		$this->yellow = $yellow;
 		$this->yellow->config->setDefault("medialistStyle", "medialist");
 	}
 	
 	// Handle page content parsing of custom block
-	function onParseContentBlock($page, $name, $text, $shortcut)
-	{
+	public function onParseContentBlock($page, $name, $text, $shortcut) {
 		$output = null;
-		if($name=="medialist" && $shortcut)
-		{
+		if($name=="medialist" && $shortcut) {
 			list($pattern, $style, $size) = $this->yellow->toolbox->getTextArgs($text);
 			if(empty($style)) $style = $this->yellow->config->get("medialistStyle");
 			if(empty($size)) $size = "100%";
 			$files = empty($pattern) ? $page->getFiles(true) : $this->yellow->files->index(true, true)->match("#$pattern#");
-			if(count($files) && $this->yellow->plugins->isExisting("image"))
-			{
+			if(count($files) && $this->yellow->plugins->isExisting("image")) {
 				$page->setLastModified($files->getModified());
 				$output = "<div class=\"".htmlspecialchars($style)."\">\n";
                     		$output .= "<ul>\n";
-				foreach($files as $file)
-				{
-
-					switch($file->get("type"))
-					{
+				foreach($files as $file) {
+					switch($file->get("type")) {
 						case "mp3":	$output .= "<li><audio controls=\"controls\">\n";
 								$output .= "<source src=\"".htmlspecialchars($file->getLocation())."\" type=\"audio/mp3\" />\n";
 								$output .= "</audio><br>\n";
@@ -58,4 +51,3 @@ class YellowMedialist
 	}
 }
 $yellow->plugins->register("medialist", "YellowMedialist", YellowMedialist::VERSION);
-?>
